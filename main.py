b6feb1208c75f37f192153df6b8b53b05723cfdb
William Chu <willcty@gmail.com>
2023-10-08 13:14:21 -0400

    transactions can be started

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ from sklearn.preprocessing import MinMaxScaler
 
 import http.client
 import requests
+import json
+import uuid
+import codecs
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Hash import SHA256
+import base64
 
 def predict():
     ticker = yf.Ticker("GAS-ETH")
@@ -79,12 +87,41 @@ def predict():
 
     fig = px.line(x=list(range(-period, pred_days)), y=lstmdf,labels={'value': 'Gas price','index': 'Timestamp'})
     fig.add_vline(x=0, line_width=3, line_dash="dash", line_color="green")
-    fig.update_layout(title_text='Plotting Ethereum Gas with prediction',
-                      plot_bgcolor='white', font_size=15, font_color='black',legend_title_text='Stock')
+    fig.update_layout(title_text='Plotting Ethereum Gas with Prediction',
+                      plot_bgcolor='white', font_size=15, font_color='black',legend_title_text='Stock',
+                      xaxis_title="Days from Today", yaxis_title="Gas")
 
     fig.write_image("images/pred.png")
 
     return np.array(lstmdf)
+
+def createNewCipherText():
+    entity_secret = bytes.fromhex(os.environ['ENTITY_SECRET'])
+
+    if len(entity_secret) != 32:
+        print("invalid entity secret")
+        exit(1)
+
+    public_key = RSA.importKey("-----BEGIN PUBLIC KEY-----\n\
+        MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAyVqtPHWiGMJEIDnnGhV8\n\
+        X8nvuw5DlUFUNhzHCkDtmNN/emSO73c6oumg6VUhJlWZPB0qw9taYlecM0PEue3m\n\
+        F0nEZBptQ54m1gLOwabtZ6pfqt42GaOjpVZ7g+BjcmEcO71rkQ8t3PPFiKU1zFgR\n\
+        UAecEc60qEkpmuJynU/hMtwaHFqThJ5fr5Cd6A3/BNmND2sHgNaKYccqDM9ecm+9\n\
+        Dgs76Wgyo3f4H21It3sNKlwYj0Ro0pqbzh8qtC09F4YRJEUna0NWQvAOYQzIjzzc\n\
+        IMg48KnJNsMgIoxVNFYSh9yPRUeVgnFtBATRhbsPf+vmaYzivZmE/6PlbWMHnfOR\n\
+        YSMYwsaUKOGSPdQE4ScccdetAU2VPRzPnEtbzPQ3TgmqoNZF6ZhlWbaHyud+RtV6\n\
+        jzfYitOe2SO7TofwBhuR0T9orxO+opFgZP9FNXjHFWmKJ5KKEZMx9LwwrVnKnFq0\n\
+        NGDUvsw6VOL0k/b8pG9X+ubC5bhSnKeyHuBgSiq3gGFWakLOJghjTA7A7capdoLf\n\
+        AgwPMRvPi0mZ9+RoUg2JfJzzLiL+AJMsTqme37suhJYjbkmq8CwMmupURTnE6AUJ\n\
+        4XH7TqcnMp0vUkyngkclcIWBbK+O4TZnaRN1WOA8sna8Wjy5HRzTO4P+ykZ5bViQ\n\
+        ZbpdAkqwIzAkje5dqFcvLCsCAwEAAQ==\n\
+        -----END PUBLIC KEY-----"
+    )
+
+    cipher_rsa = PKCS1_OAEP.new(key=public_key, hashAlgo=SHA256)
+    encrypted_data = cipher_rsa.encrypt(entity_secret)
+    encrypted_data_base64 = base64.b64encode(encrypted_data)
+    return encrypted_data_base64.decode()
 
 app = Flask(__name__)
 
@@ -123,14 +160,27 @@ def sms_reply():
 
         conn.request("GET", f"/v1/w3s/wallets/{os.environ['WALLET_1_ID']}/balances", headers=headers)
         res = conn.getresponse()
-        data = res.read()
+        data = json.loads(res.read().decode('utf-8'))
+
+        balanceString = None
+        for token in data['data']['tokenBalances']:
+            tokenID = token['token']['id']
+            amount= token['amount']
+            unit = token['token']['name']
+
+            tokenString = f"Token ID: {tokenID}\nAmount: {amount} {unit}"
+            if balanceString is None:
+                balanceString = tokenString
+            else:
+                balanceString += f"\n\n{tokenString}"
 
         message = client.messages \
             .create(
-                 body=f'{data.decode("utf-8")}',
+                 body=balanceString,
                  from_='+18442608697',
                  to='+18482189972'
              )
+
 
     elif body == '!transfer':
         url = "https://api.circle.com/v1/w3s/transactions/transfer/estimateFee"
@@ -160,7 +210,36 @@ def sms_reply():
         print("High Max Fee:", high_max_fee)
         message = client.messages \
             .create(
-                 body=f'Transfer\n\nEstimated Gas:\nLow Max Fee:{low_max_fee}\nLow Max Fee:{medium_max_fee}\nLow Max Fee:{high_max_fee}\n\nConfirm?\n\nFunctionality NOT finished',
+                 body=f'Transfer 1 USDC\n\nEstimated Gas:\nLow Max Fee:{low_max_fee}\nLow Max Fee:{medium_max_fee}\nLow Max Fee:{high_max_fee}\n\nConfirm?',
+                 from_='+18442608697',
+                 to='+18482189972'
+             )
+
+    elif body=='yes':
+
+        url = "https://api.circle.com/v1/w3s/developer/transactions/transfer"
+
+        payload = {
+            "amounts": ["1"],
+            "idempotencyKey": str(uuid.uuid4()),
+            "destinationAddress": os.environ['WALLET_2_ADDRESS'],
+            "entitySecretCiphertext": createNewCipherText(),
+            "tokenId": os.environ['SAMPLE_ETH_TOKEN_ID'],
+            "walletId": os.environ['WALLET_1_ID'],
+            "feeLevel": "LOW"
+        }
+
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "authorization": "Bearer TEST_API_KEY:d237343fb44c9582c0280e5f43297d8d:017d2bf54597051de1d4e1439a44f6b1"
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        message = client.messages \
+            .create(
+                 body=f"Transfer Initiated\n{response.json()['data']['id']}",
                  from_='+18442608697',
                  to='+18482189972'
              )
